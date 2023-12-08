63a9eb3165ef438ff17b5987e1af071da2fc7a2f
Themelis Kalomoiros <arrditis@gmail.com>
2023-12-08 16:33:04 +0100

    Added SongInfo.__str__

--- song_info.py
+++ song_info.py
@@ -2,20 +2,45 @@ from models import Song, Artist
 
 
 class SongInfo:
-    def __init__(self):
-        self.song = None
+    def __init__(
+        self, 
+        title, 
+        lyrics, 
+        year = None, 
+        youtube = None, 
+        spotify = None):
+        self.song = Song(title, lyrics, year, youtube, spotify)
         self.authors = set()
         self.singers = set()
         self.composers = set()
 
 
     def add_author(self, author):
-        self.authors.append(author)
+        self.authors.add(author)
 
 
     def add_singer(self, singer):
-        self.singers.append(singer)
+        self.singers.add(singer)
 
 
     def add_composer(self, composer):
-        self.composers.append(composer)
+        self.composers.add(composer)
+
+
+    def __str__(self):
+        s = f'Title: {self.song.title}\nYear: {self.song.year}'
+        s += f'\nYoutube: {self.song.youtube}'
+        s += f'\nSpotify: {self.song.spotify}'
+        s += f'\nLyrics: \n{self.song.lyrics}'
+
+        s += '\n\nAuthors:'
+        for a in self.authors:
+            s += f'\n{a}'
+        s += '\n\nComposers:'
+        for a in self.composers:
+            s += f'\n{a}'
+        s += '\n\nSingers:'
+        for a in self.singers:
+            s += f'\n{a}'
+
+        return s
