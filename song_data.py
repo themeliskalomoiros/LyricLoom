54b2167ffd3451796a40435ea1146de51ca229cc
Themelis Kalomoiros <arrditis@gmail.com>
2023-12-08 13:30:57 +0100

    SongData switches from lists to dictionaries

--- song_data.py
+++ song_data.py
@@ -4,6 +4,18 @@ from models import Song, Artist
 class SongData:
     def __init__(self):
         self.song = None
-        self.authors = []
-        self.singers = []
-        self.composers = []
+        self.authors = set()
+        self.singers = set()
+        self.composers = set()
+
+
+    def add_author(self, author):
+        self.authors.append(author)
+
+
+    def add_singer(self, singer):
+        self.singers.append(singer)
+
+
+    def add_composer(self, composer):
+        self.composers.append(composer)
