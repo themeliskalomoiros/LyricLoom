5b731ec370f313648b071ac881e12438bfbbbc7c
Themelis Kalomoiros <arrditis@gmail.com>
2023-12-11 09:46:59 +0100

    Redefined SongScraper + exceptions print their own message

--- song_scraper.py
+++ song_scraper.py
@@ -4,18 +4,93 @@ from abc import ABC, abstractmethod
 
 
 class SongScraper(ABC):
+    def __init__(self, url):
+        self.url = url
+        self.soup = page_soup()
+
+
+    def page_soup(self, url):
+        response = requests.get(url)
+        if (response.status_code == 200):
+            return BeautifulSoup(response.content, 'html.parser')
+        else:
+            raise PageNotLoadedException(f'Error requesting {url}')
+
+
     @abstractmethod
-    def scrap_song_info(self) -> SongInfo:
+    def get_song_info(self) -> SongInfo:
+        pass
+
+
+    @abstractmethod
+    def scrap_title(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_lyrics(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_release_date(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_youtube_url(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_spotify_url(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_guitar_tabs(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_duration(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_authors(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_composers(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_singers(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_rhythms(self):
+        pass
+
+
+    @abstractmethod
+    def scrap_scales(self):
         pass
 
 
 
 class PageNotLoadedException(Exception):
-    """Thrown when a web page could not be loaded."""
-    pass
+    def __init__(self, message='An error occurred while loading page'):
+        self.message = message
+        super().__init__(self.message)
 
 
 
 class ScrapException(Exception):
-    """Thrown when something went wrong during the scraped of the data."""
-    pass
+    def __init__(self, message='An error occurred while scrapping'):
+        self.message = message
+        super().__init__(self.message)
