fc33efccb5a45a9c025cffe87d68ec32a0ee51ed
Themelis Kalomoiros <arrditis@gmail.com>
2023-12-11 18:14:04 +0100

    App creates a db file if it doesnt already exist

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+import os
+import sqlite3
 from sql_repo import SqlRepo
 from progress_bar import printProgressBar
 from domna_samiou_scraper import DomnaSamiouScraper
 from song_scraper import PageNotLoadedException, ScrapException
 from song_repo import SongAlreadyExistException, InvalidArtistException, InvalidTagException
+
+
+db_file = 'lyrics.db'
+
+def create_db_if_not_exist():
+    schema_file = 'schema.sql'
+
+    if not os.path.exists(db_file):
+        with sqlite3.connect(db_file) as conn:
+            cursor = conn.cursor()
+
+            with open(schema_file, 'r') as schema_file:
+                schema_script = schema_file.read()
+                cursor.executescript(schema_script)
+
+            conn.commit()
+
+        print(f'Created database file {db_file}.')
 
 
 def log(successful_insertions, failed_insertions):
@@ -22,11 +42,13 @@ def urls(song_web_ids):
 
 
 def domna_samiou_gr():
+    create_db_if_not_exist()
     progress = 1
-    progressMax = 930
+    # TODO: Set to 930
+    progressMax = 5
     insertions = 0
     failed_insertions = 0
-    repo = SqlRepo('stixoi.db')
+    repo = SqlRepo(db_file)
 
 
     for url in urls(range(progress, progressMax)):
@@ -41,25 +63,22 @@ def domna_samiou_gr():
 
         try:
             scraper = DomnaSamiouScraper(url)
-            info = scraper.scrap_song_info()
-            repo.save(info)
+            scraper.scrap_title()
+            scraper.scrap_lyrics()
+            scraper.scrap_youtube_url()
+            scraper.scrap_spotify_url()
+            scraper.scrap_authors()
+            scraper.scrap_composers()
+            scraper.scrap_singers()
+            scraper.scrap_spotify()
+            scraper.scrap_tags()
+
+            repo.save(scraper.get_song_info())
             insertions += 1
             if insertions % 100 == 0:
                 repo.commit()
 
-        except ScrapException:
-            failed_insertions += 1
-            log(insertions, failed_insertions)
-        except PageNotLoadedException:
-            failed_insertions += 1
-            log(insertions, failed_insertions)
-        except SongAlreadyExistException:
-            failed_insertions += 1
-            log(insertions, failed_insertions)
-        except InvalidArtistException:
-            failed_insertions += 1
-            log(insertions, failed_insertions)
-        except InvalidTagException:
+        except (ScrapException, PageNotLoadedException, SongAlreadyExistException, InvalidArtistException, InvalidTagException):
             failed_insertions += 1
             log(insertions, failed_insertions)
 
